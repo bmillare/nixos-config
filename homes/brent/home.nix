1c8bd712580bf41c1e6e44ecd6b5d372e83246f3
Brent Millare <brent.millare@gmail.com>
2026-06-14 07:53:00 -0700

    next

--- homes/brent/home.nix
+++ homes/brent/home.nix
@@ -1,8 +1,36 @@
 { pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
-  users.users.brent.extraGroups = [ "docker" ];
+  #virtualisation.docker.enable = true;
+  virtualisation.podman.enable = true;
+  environment.systemPackages = [ pkgs.distrobox ];
+  #users.users.brent.extraGroups = [ "docker" ];
+
+  # virtualisation.oci-containers = {
+  #   backend = "docker";
+  #   containers = {
+  #     ubuntu-server = {
+  #       image = "ubuntu:latest";
+  #       cmd = [ "sleep" "infinity" ];
+  #     };
+  #   };
+  # };
+  # virtualisation.oci-containers = {
+  #   backend = "docker";
+  #   containers = {
+  #     vanta-ubuntu = {
+  #       image = "jrei/systemd-ubuntu:latest";
+  #       # Systemd requires privileged access to manage system services
+  #       extraOptions = [ "--privileged" ];
+  #       volumes = [
+  #         "/sys/fs/cgroup:/sys/fs/cgroup:ro"
+  #       ];
+  #     };
+  #   };
+  # };
+  users.users.brent.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzv9piBq1YBoem21fGSuNUQO9JfbpOusARoSyojJ6wH brent@psynk.ai"
+    ];
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
@@ -17,15 +45,39 @@
        pkgs.docker-compose
     ];
 
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+
+      # work github is default
+      settings."github.com" = {
+        HostName = "github.com";
+        User = "git";
+        IdentityFile = "~/.ssh/brent_spynk_ai_ed25519_key";
+        IdentitiesOnly = true;
+      };
+      settings."github.com_personal" = {
+        HostName = "github.com";
+        User = "git";
+        IdentityFile = "~/.ssh/id_ed25519";
+        IdentitiesOnly = true;
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      settings = {
+        init.defaultBranch = "main";
+      };
+    };
+
     programs.home-manager.enable = true;
     programs.bash.enable = true;
     programs.firefox.enable = true;
 
-    programs.git = {
+    programs.tmux = {
       enable = true;
-      extraConfig = {
-        init.defaultBranch = "main";
-      };
     };
+
   };
 }
